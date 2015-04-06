3c9455db9c38e2c7f89c8fa0a44f6f10313d81b3
Martin Kikegawa <mkikegawa@gmail.com>
2015-04-05 21:50:36 -0400

    stopped the user marker from moving with center

--- client/app/scripts/controllers/restaurants.js
+++ client/app/scripts/controllers/restaurants.js
@@ -2,82 +2,54 @@
 
 angular.module('clientApp')
 .controller('RestaurantsCtrl', ['$http', '$scope', 'RestaurantsService', function($http, $scope, RestaurantsService) {
-  
-
-  $scope.map = {
-    center: {
-      latitude:  33.7722818, 
-      longitude: -84.36655619999999
-    },
-    zoom: 15,
-   };
-
-
-  // $scope.marker = {
-  //     coords: {
-  //       latitude: 33.7722818,
-  //       longitude: -84.56655619999999
-  //     }
-  //   };
+  $scope.restaurantMarkers = [];
+  function initialize () {
+    var coords = {
+        latitude: 33.7722818,
+        longitude: -84.3665563
+    };
+    makeRestaurantMarkers(restaurants);
+  }
 
 
    
-   var onSuccess = function(position) {
-    $scope.map.center = {
+  var onSuccess = function(position) {
+    var coords = {
         latitude: position.coords.latitude,
         longitude: position.coords.longitude
     };
-    $scope.marker = {
+    console.log(coords);
+    $scope.map = {
+      center: coords,
+      zoom: 15,
+    };
+    $scope.marker = angular.isUndefined($scope.marker) ? {  
       idKey: 0,
-      coords: {
-        latitude: position.coords.latitude,
-        longitude: position.coords.longitude
-      }
-    };
-    
-    $scope.$apply();
-  };
+      coords: coords
+    } : $scope.marker;
 
-  // var createMarker = function(position) {
-  //   $scope.marker = {
-  //     idKey: 0,
-  //     coords: {
-  //       latitude: position.coords.latitude,
-  //       longitude: position.coords.longitude
-  //     }
-  //   };
-  //   $scope.apply();
-  // };
+    var location = coords.latitude.toString() + ',' + coords.longitude.toString();
+    var url = '/api/restaurants.json?location=' + location;
+    console.log(url);
+    $scope.getRestaurants(url);
+  };
 
   function makeRestaurantMarkers(restaurants) {
     var list = [];
-    console.log(restaurants[0]);
+    console.log('restaurant: ' + restaurants);
     var arrayLength = restaurants.length;
-    console.log(arrayLength);
+    console.log('arrayLength: ' + arrayLength);
     for (var i=0; i < arrayLength; i++) {
       list.push({
         id: restaurants[i].place_id,
         latitude: restaurants[i].geometry.location.lat,
         longitude: restaurants[i].geometry.location.lng
-
       });
     }
-
-    // var i = 0;
-    // while (restaurants.results[i] !== null ) {
-    //   list.push({
-    //     id: restaurants.results[i].place_id,
-    //     latitude: restaurants.results[i].geometry.location.lat,
-    //     longitude: restaurants.results[i].geometry.location.lng
-        
-    //   });
-    //   i++;
-    // }
-
     $scope.restaurantMarkers = list;
 
   }
-
+  
 
   function onError(error) {
       console.log('code: ' + error.code  + '\n' + 'message: ' + error.message + '\n');
@@ -85,25 +57,22 @@ angular.module('clientApp')
 
   navigator.geolocation.getCurrentPosition(onSuccess);
 
-   // plug the geolocate data into the location variable
-  var location = '33.7722636,-84.3661896';
-
-  var url = '/api/restaurants.json?location=' + location;
-
+  
 
   $scope.getRestaurants = function (url) {
 
     RestaurantsService.getRestaurants(url) 
     .success(function(data) {
-      $scope.restaurants = data.results;
-      makeRestaurantMarkers(data.results);
-      console.log('data.results');
-      console.log($scope.restaurantMarkers);
+      var results = data.results;
+      // console.log('data.results: ' + data.status);
+      console.log('data.results: ' + data.results);
+      $scope.restaurants = results;
+      makeRestaurantMarkers(results);
     })
     .error(function(data) {
       console.log('error --->\n' + data);
     });
   };
 
-  $scope.getRestaurants(url);
+  
 }]);
