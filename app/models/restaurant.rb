5a7a5725f7dbd9eade32156867adb0e07874c558
Martin Kikegawa <mkikegawa@gmail.com>
2015-04-07 12:13:07 -0400

    reworked restaurant search - ensure 5 restaurant filled

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,8 +3,8 @@ class Restaurant #< ActiveRecord::Base
     location ||= '33.7722636,-84.3661896'
     base = "https://maps.googleapis.com/maps/api/"
     search_type = "place/nearbysearch/json"
-    # query = "?location=" + location + "&radius=1200&opennow=true&types=restaurant&rating=3"
-    query = "?location=" + location + "&radius=1200&types=restaurant&rating=3"
+    query = "?location=" + location + "&radius=1200&opennow=true&types=restaurant&rating=3"
+    # query = "?location=" + location + "&radius=1200&types=restaurant&rating=3"
     key = '&key=AIzaSyA5NOMzV1J0S1nADTkc06UrFh_n7JUA9yU'
     
     query_url = base + search_type + query + key
--- client/app/scripts/controllers/restaurants.js
+++ client/app/scripts/controllers/restaurants.js
@@ -6,9 +6,9 @@ angular.module('clientApp')
   $scope._ = _;   
   $scope.restaurants = [];
   $scope.restaurantMarkers = [];
-  $scope.userPositionSet = false;
-  // this function sets up the Original marker and map center
-  var onSuccess = function(position) {
+
+  // this function sets up initial map center
+  var centerMap = function(position) {
 
     var coords = {
         latitude: position.coords.latitude,
@@ -21,10 +21,17 @@ angular.module('clientApp')
       center: coords,
       zoom: 15,
     };
+  };
 
-    $scope.marker = $scope.userPositionSet ? $scope.marker : makeMarker(coords);
-    
-    // $scope.marker = isUserStationary(position) ?  : $scope.marker;
+  // this function sets up the User position marker 
+  var userPosition = function(position) {
+    var coords = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude
+    };
+        
+    $scope.marker = makeMarker(coords);
+   
     // set up api query string using current location as center of search
     var location = coords.latitude.toString() + ',' + coords.longitude.toString();
     var url = '/api/restaurants.json?location=' + location;
@@ -38,14 +45,10 @@ angular.module('clientApp')
     return obj;
   }
 
-  // function isUserStationary(position) {
-  //   console.log('_.negate(_.isNull(position.coords.speed)) ' + _.negate(_.isNull(position.coords.speed)));
-  //   return angular.isUndefined($scope.marker) || _.negate(_.isNull(position.coords.speed));
-  // }
-
-
     // Html 5 Api function to get position 
-  navigator.geolocation.getCurrentPosition(onSuccess);
+  navigator.geolocation.getCurrentPosition(centerMap);
+
+  navigator.geolocation.watchPosition(userPosition);
 
   // function create markers from a list of restaurants
   function makeRestaurantMarkers(restaurants) {
@@ -54,9 +57,9 @@ angular.module('clientApp')
 
     // console.log('in makeRestaurantMarkers restaurant: ' + JSON.stringify(restaurants));
     var arrayLength = restaurants.length;
-    console.log('arrayLength: ' + arrayLength);
+    console.log('restaurantMarker arrayLength: ' + arrayLength);
     // JSON 
-    for (var i=0; i < arrayLength; i++) {
+    for (var i=0; i < 5; i++) {
       list.push({
         name: restaurants[i].name,
         id: restaurants[i].place_id,
@@ -112,9 +115,33 @@ angular.module('clientApp')
       console.log('choiceLevel: ' + JSON.stringify(choiceLevel));
       return priceChooser && restaurant.rating >= choiceLevel.rating;
     }));
-    makeRestaurantMarkers(priceFiltered);
+
+    // ensure a unique object array [priceFiltered] while filling up to length of 5 
+    // adding to array using sort order by rating then price
+    console.log('priceFiltered length: ' + priceFiltered.length);
+    
+
+    if (priceFiltered.length < 5) {
+      var tempPriceFiltered = priceFiltered;
+      var presort = restaurantSort($scope.restaurantRaw);
+      var deleteTest;
+      console.log('presort: ' + JSON.stringify(presort));
+
+      for (var i=0; i < $scope.restaurantRaw.length; i++) {
+        deleteTest = tempPriceFiltered.push(presort[i]);
+        priceFiltered = _.uniq(deleteTest, 'results.place_id');
+      }
+    } 
+    console.log('priceFiltered: ' + JSON.stringify(priceFiltered));
+    makeRestaurantMarkers(restaurantSort(priceFiltered));
   };
 
+  
+  function restaurantSort (restaurants) {
+    return _.sortByOrder(restaurants, ['rating','price_level'], [false, false]);
+  }
+
+    
   // get data from google places via rails server
   $scope.getRestaurants = function (url) {
     RestaurantsService.getRestaurants(url) 
